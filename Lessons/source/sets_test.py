45e11115c50547fbe86939c8a5957f21874cfc9a
Stephen Ouyang <xisouyang@gmail.com>
2019-04-23 17:04:00 -0700

    [add] added test for union function

--- Lessons/source/sets_test.py
+++ Lessons/source/sets_test.py
@@ -49,3 +49,15 @@ class SetsTest(unittest.TestCase):
         assert s.hash_set.size == 2
         with self.assertRaises(KeyError):
             s.remove('A')
+
+    def test_union(self):
+
+        set_one = Set(['A', 'B', 'C'])
+        set_two = Set(['X', 'Y'])
+        union_set = set_one.union(set_two)
+        self.assertCountEqual(union_set.hash_set.keys(), ['A', 'B', 'C', 'X', 'Y'])
+
+        set_three = Set(['D', 'F', 'G'])
+        set_four = Set(['F', 'G', 'H'])
+        union_set = set_three.union(set_four)
+        self.assertCountEqual(union_set.hash_set.keys(), ['D', 'F', 'G', 'H'])
